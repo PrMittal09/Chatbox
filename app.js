e24dc7bc3553cf2ae4b46abea05d9834b75d3317
PrMittal09 <prateekmittal075@gmail.com>
2020-02-19 01:18:01 +0530

    updating app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(function(req, res, next) {
 });
 
 let http = require('http');
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('port',port);
 
 //use dict directory of angular project folder
